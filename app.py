3bf438271aa05ea9bd9c4db967ba6dcf28fa60a5
Dishant Padalia <64226014+dishant26@users.noreply.github.com>
2022-07-08 10:14:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Query, Path, File, UploadFile, Form, Depends
+from fastapi import Depends, FastAPI, File, Form
+from pydantic import BaseModel, validator, BaseSettings, Json
 from starlette.requests import Request
 from pydantic import BaseModel
 from typing import Optional
@@ -16,21 +18,18 @@ from indicparser import indic_parser
 
 app = FastAPI()
 
-class Item(BaseModel):
-    inference: str
-    lang: str
-    confidence_threshold: Optional[float] = None
-    model: Optional[str] = None
-
-
 @app.post('/ocr')
-def upload_file(file: UploadFile, config: Item = Depends()):
+async def upload_file(file: UploadFile, 
+                      inference: str = Form(...), 
+                      lang: str = Form(...), 
+                      model: Optional[str] = Form(None), 
+                      confidence_threshold: Optional[float] = Form(None)):
     print(file.filename)
     with open("temporary.png", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     img = cv2.imread("temporary.png", cv2.IMREAD_GRAYSCALE)
     img2 = cv2.imread("temporary.png", cv2.IMREAD_COLOR)
     pil_file=Image.fromarray(img)
-    output = indic_parser(config.inference, config.lang, pil_file, file.filename, config.model, config.confidence_threshold, img2)
+    output = indic_parser(inference, lang, pil_file, file.filename, model, confidence_threshold, img2)
     return output
  
